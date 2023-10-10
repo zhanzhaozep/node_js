cdf3142a868a3351067730ba0c437993f15c68ef
zhanzhaozep <220044jg@yse-c.net>
2023-10-10 09:23:57 +0900

    node_js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Express MVCフレームワーク（Webフレームワーク）
 // expressモジュール読み込み
 const express = require('express')
 // dotenvモジュール読み込み
@@ -8,13 +9,15 @@ dotenv.config()
 const HOST = process.env.HOST
 const PORT = process.env.PORT
 
-console.log(HOST)
-console.log(PORT)
 
+//Webサーバ作成
 const app = express()
 
-app.get('/', (req, res)=> {
-
+// ルーティング（URLマッピング）
+// トップページにGETリクエストされたとき処理
+// サーバを停止（ていし）: Ctrl + C
+app.get('/', (req, res) => {
+    // クライアンのからのリクエスト
     console.log(req.body)
     console.log(req.url)
     console.log(req.query)
@@ -23,6 +26,11 @@ app.get('/', (req, res)=> {
     res.send('Hello!!!!')
 })
 
+app.get('/profile', (req, res) => {
+    res.send('Profile Page!')
+})
+
+// サーバー待機（たいき） wait 
 app.listen(PORT, HOST, () => {
     console.log(HOST)
     console.log(PORT)
